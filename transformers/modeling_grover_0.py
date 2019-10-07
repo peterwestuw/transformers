89474dfe74aa65307ff1ec1fed657eb2910f1998
peterwestuw <peter.west@alumni.ubc.ca>
2019-10-07 14:59:44 -0700

    config fixes

--- transformers/modeling_grover_0.py
+++ transformers/modeling_grover_0.py
@@ -448,7 +448,7 @@ class GroverModel(GroverPreTrainedModel):
         self.output_hidden_states = True #config.output_hidden_states
         self.output_attentions = True #config.output_attentions
 
-        self.wte = nn.Embedding(config.vocab_size, config.n_embd)
+        self.wte = nn.Embedding(config.vocab_size, config.hidden_size)#config.n_embd)
         self.wpe = nn.Embedding(config.max_position_embeddings, config.hidden_size)#  n_positions, config.n_embd)
         self.drop = nn.Dropout(config.hidden_dropout_prob) #embd_pdrop)
         self.h = nn.ModuleList([Block(config.n_ctx, config, scale=True) for _ in range(config.num_hidden_layers)])# n_layer)])
